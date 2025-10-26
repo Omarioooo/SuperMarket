98bb0fed0fedd4a6fd5c850fa66b4faba12fe115
Omar Mohamed <om1963448@gmail.com>
2025-10-26 17:36:55 +0300

    Update branch

--- obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
+++ obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57f0de98d594a0f77d5cb6e75171b50748face2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
