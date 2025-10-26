4f1bdbc697681daa574edc929a7256b6a7a2db90
Omar Mohamed <om1963448@gmail.com>
2025-10-27 01:35:33 +0300

    Market Controller are done...

---
+++ Controllers/MarketController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Market")]
+    public class MarketController : ControllerBase
+    {
+        private readonly IMarketService _marketService;
+
+        public MarketController(IMarketService marketService)
+        {
+            _marketService = marketService;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CreateProduct(ProductDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var product = await _marketService.CreateProductAsync(model);
+
+            if (product == null)
+                return BadRequest($"Field to add {model.Name}");
+
+            return Ok(product);
+        }
+
+        [HttpPut("[action]/{id:int}")]
+        public async Task<IActionResult> UpdateProduct(int id, ProductDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var product = await _marketService.UpdateProductAsync(id, model);
+
+            if (product == null)
+                return BadRequest($"Field to update {model.Name}");
+
+            return Ok(product);
+        }
+
+        [HttpDelete("[action]/{id:int}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            bool isDeleted = await _marketService.RemoveProductAsync(id);
+
+            if (!isDeleted)
+                return BadRequest($"Field to remove");
+
+            return Ok("Deleted Successfully");
+        }
+    }
+}
--- obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
+++ obj/Debug/net9.0/SuperMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bb0fed0fedd4a6fd5c850fa66b4faba12fe115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee28dee154199a0553ca819bf3f7742c97432cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
